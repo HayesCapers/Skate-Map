4986e953325ae4d21fa6023c2d6fcc038ecd6e39
Nicolas Blumenstock <nicpblum@gmail.com>
2017-08-09 13:40:56 -0400

    cleaning up files

--- front/src/components/Camera.js
+++ front/src/components/Camera.js
@@ -9,40 +9,6 @@ import { Actions } from 'react-native-router-flux';
 
 
 class Cam extends Component {
-	
-	// sendPic() {
-
-	// 	const file = {
-	// 			uri: this.state.imagePath,
-	// 			name: 'delaytest.jpg',
-	// 			type: 'image/jpg'
-	// 		}
-
-	// 		const options = {
-	// 			keyPrefix: "images/",
-	// 			bucket: "s3-skatespot",
-	// 			region: "us-east-1",
-	// 			accessKey: "AKIAIKYBFCBIKMB4SLYA",
-	// 			secretKey: "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",
-	// 			successActionStatus: 201
-	// 		}
-	// 		RNS3.put(file, options).then(response => {
-	// 			if (response.status !== 201)
-	// 				throw new Error("Failed to upload image to S3");
-	// 			console.log(response.body);
-	// 			/**
-	// 			* {
-	// 			*   postResponse: {
-	// 			*     bucket: "your-bucket",
-	// 			*     etag : "9f620878e06d28774406017480a59fd4",
-	// 			*     key: "uploads/image.png",
-	// 			*     location: "https://your-bucket.s3.amazonaws.com/uploads%2Fimage.png"
-	// 			*   }
-	// 			* }
-	// 			*/
-	// 		});
-
-	// }
 
 	takePicture() {
 		this.camera.capture()
@@ -111,15 +77,6 @@ const styles = {
 	}
 }	 
 
-// { path: 'assets-library://asset/asset.JPG?id=6FBD4E72-C9C0-4DC9-9008-03FEDFC57FAB&ext=JPG',
-//   mediaUri: 'assets-library://asset/asset.JPG?id=6FBD4E72-C9C0-4DC9-9008-03FEDFC57FAB&ext=JPG' } 
-
-// flex: 1,
-// justifyContent: 'flex-end',
-// alignItems: 'center',
-// height: 350,
-// width: 350
-// <Image source={{uri: this.state.image, scale: 1}} style={{ height: 100, width: 100 }} />
 
 export default connect(null,{ takePic })(Cam);
 
